a8576f2c9f90784a4864cb837ad2bb86a1b2238c
worsleyj <worsleyj23@gmail.com>
2025-03-26 23:57:28 -0400

    display current input values and remove required

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
 <body>
     <form action="index.php" method="post">
         <label>Email</label>
-        <input type="text" name="email" required>
+        <input type="text" name="email">
         <br>
         <label>Username</label>
-        <input type="text" name="username" required>
+        <input type="text" name="username">
         <br>
         <label>Password</label>
-        <input type="text" name="password" required>
+        <input type="text" name="password">
         <br>
         <label>Confirm Password</label>
-        <input type="text" name="password_confirm" required>
+        <input type="text" name="password_confirm">
         <br>
         <input type="submit" value="Sign Up">
     </form>
@@ -25,5 +25,13 @@
 </html>
 
 <?php
-    echo $_POST["email"];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password_confirm = $_POST['password_confirm'];
+
+    echo "Email: $email <br>";
+    echo "Username: $username <br>";
+    echo "Password: $password <br>";
+    echo "Confirm Password: $password_confirm <br>";
 ?>
